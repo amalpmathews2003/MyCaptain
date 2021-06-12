1950a0d139652e64ec806292821e28bb27f591cd
Amal P Mathews <amalpmathews2003@gmail.com>
2021-06-12 10:21:26 +0530

    Create sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char a='A';
+	int b=1;
+	float c=3.14;
+	double d=4.6744;
+	cout<<"Size of Char : "<<sizeof(a)<<" Byte\n";
+	cout<<"Size of Int : "<<sizeof(b)<<" Byte\n";
+	cout<<"Size of Float : "<<sizeof(c)<<" Byte\n";
+	cout<<"Size of Double : "<<sizeof(d)<<" Byte\n";
+
+	return 0;
+}
